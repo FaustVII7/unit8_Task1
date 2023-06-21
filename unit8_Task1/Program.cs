6b3290f5f14a486f8963bed99ecace066ff65c9c
FaustVII7 <vaslykar@yandex.ru>
2023-06-21 21:35:37 +0300

    Добавьте файлы проекта.

---
+++ unit8_Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace FolderCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до папки для очистки:");
+            string folderPath = Console.ReadLine();
+
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(folderPath);
+                if (!directory.Exists)
+                {
+                    Console.WriteLine("Папки по заданному пути не существует.");
+                    return;
+                }
+
+                CleanFolder(directory, TimeSpan.FromMinutes(30));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static void CleanFolder(DirectoryInfo directory, TimeSpan threshold)
+        {
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if (DateTime.UtcNow - file.LastAccessTimeUtc > threshold)
+                {
+                    file.Delete();
+                    Console.WriteLine($"Удалён файл: {file.FullName}");
+                }
+            }
+
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+            {
+                CleanFolder(subDirectory, threshold);
+
+                if (subDirectory.GetFiles().Length == 0 && subDirectory.GetDirectories().Length == 0 &&
+                    DateTime.UtcNow - subDirectory.LastAccessTimeUtc > threshold)
+                {
+                    subDirectory.Delete();
+                    Console.WriteLine($"Удалена пустая папка: {subDirectory.FullName}");
+                }
+            }
+        }
+    }
+}
